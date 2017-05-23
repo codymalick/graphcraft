0ba85551f6457b21f43c4e0392cf65d2031d6abd
codymalick <cody.malick@gmail.com>
2017-05-23 16:58:56 -0700

    basic calls working

---
+++ getJson.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	//"net/url"
+	"net/http"
+	"log"
+	"os"
+	"encoding/json"
+)
+
+// Calls the Blizzard community API to get periodic data dumps.
+
+func BuildItemQueryString(locale string, apiKey string, id string) string {
+	url, err := http.NewRequest("GET", BASE_URL + ITEM_URL + id, nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	query := url.URL.Query()
+	query.Add("apikey",apiKey)
+
+	query.Add("locale",locale)
+
+	url.URL.RawQuery = query.Encode()
+
+	println(url.URL.String())
+
+	return url.URL.String()
+}
+
+func BuildAuctionLocationQueryString(locale string, apiKey string, realm string) string {
+	url, err := http.NewRequest("GET", BASE_URL + AUCTION_URL + DATA + realm, nil)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	query := url.URL.Query()
+	query.Add("apikey",apiKey)
+
+	query.Add("locale",locale)
+
+	url.URL.RawQuery = query.Encode()
+
+	println(url.URL.String())
+
+	return url.URL.String()
+}
+
+func GetItemRequest(request string, item *Item) {
+	response,err := http.Get(request)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(item)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+}
+
+func GetAuctionLocationRequest(request string, auction *AuctionLocation) {
+	response,err := http.Get(request)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(auction)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+}
+
+func GetAuctionRequest(request string, auction *Auction) {
+	response,err := http.Get(request)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(auction)
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-
+import (
+	"flag"
+)
+const (
+	BASE_URL = "https://us.api.battle.net/wow"
+	AUCTION_URL = "/auction/"
+	ITEM_URL = "/item/"
+	EN_US_LOCALE = "en_US"
+	DATA = "/data/"
+)
 
 func main() {
-	// Read api key from 'secret' file
+
+	// Read api key from cli
+	apiKey := flag.String("a", "00000000", "-a <key>")
+
+	flag.Parse()
+
+	// Call the item API
+
+	requestUrl := BuildItemQueryString(EN_US_LOCALE, *apiKey, "18802")
+
+	item := new(Item)
+	GetItemRequest(requestUrl, item)
+
+	println(len(item.ItemSpells))
+
+	auctionUrl := BuildAuctionLocationQueryString(EN_US_LOCALE, *apiKey, "Emerald Dream")
+
+	locationUrl := new(AuctionLocation)
+
+	GetAuctionLocationRequest(auctionUrl, locationUrl)
+
+	println(locationUrl.Files[0].URL)
+
+	auction := new(Auction)
+
+	GetAuctionRequest(locationUrl.Files[0].URL, auction)
+
+	for index,auc := range auction.Auctions {
+		println(index,auc.Item)
+	}
+
+
 }
